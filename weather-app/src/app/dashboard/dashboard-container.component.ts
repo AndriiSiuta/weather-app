fdbfc31001cf45cc91d0d016f574caea3c05a542
Oleksandr Dudynets <hello@dudynets.dev>
2023-08-24 23:40:50 +0300

    feature(ui): completely redesign the app, add prettier, add an ability to remove saved users

--- weather-app/src/app/dashboard/dashboard-container.component.ts
+++ weather-app/src/app/dashboard/dashboard-container.component.ts
@@ -1,21 +1,27 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
-import { WeatherStateService } from '@core/weather-state.service';
-import { UserStateService } from '@core/user-state.service';
-import { map, mergeMap, Observable, switchMap, toArray } from 'rxjs';
-import { IWeatherState } from '@core/model';
-import { LocalStorageService } from '@core/localStorage.service';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import {WeatherStateService} from '@core/weather-state.service';
+import {UserStateService} from '@core/user-state.service';
+import {map, mergeMap, Observable, switchMap, toArray} from 'rxjs';
+import {IWeatherState} from '@core/model';
+import {LocalStorageService} from '@core/localStorage.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-dashboard-container',
   templateUrl: './dashboard-container.component.html',
   styleUrls: ['./dashboard-container.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-
 export class DashboardContainerComponent implements OnInit {
   weatherStateService = inject(WeatherStateService);
   userStateService = inject(UserStateService);
-  lsService = inject(LocalStorageService)
+  lsService = inject(LocalStorageService);
+  router = inject(Router);
   state$!: Observable<IWeatherState[]>;
   bhState$ = this.lsService.state;
 
@@ -23,24 +29,40 @@ export class DashboardContainerComponent implements OnInit {
     this.state$ = this.userStateService.getUserState().pipe(
       switchMap((users) => {
         return users.map((user) => {
-          return this.weatherStateService.getWeatherState({
-            longitude: +user.location.coordinates.longitude,
-            latitude: +user.location.coordinates.latitude
-          }).pipe(
-            map((weather) => ({
-              user: user,
-              weather: weather,
-              saved: false
-            }))
-          )
-        })
+          return this.weatherStateService
+            .getWeatherState({
+              longitude: +user.location.coordinates.longitude,
+              latitude: +user.location.coordinates.latitude,
+            })
+            .pipe(
+              map((weather) => ({
+                user: user,
+                weather: weather,
+                saved: false,
+              })),
+            );
+        });
       }),
       mergeMap((w) => w),
-      toArray()
-    )
+      toArray(),
+    );
+  }
+
+  handleLocation(state: IWeatherState) {
+    this.router.navigate(['map-layer'], {
+      queryParams: {
+        lat: state.weather.latitude,
+        lon: state.weather.longitude,
+        userPicture: encodeURIComponent(JSON.stringify(state.user.picture)),
+      },
+    });
   }
 
   handleSave(state: IWeatherState) {
     this.lsService.saveItem(state);
+  }
+
+  handleRemove($event: IWeatherState) {
+    this.lsService.removeItem($event);
   }
 }
--- weather-app/src/app/ui-kit/header/header.component.ts
+++ weather-app/src/app/ui-kit/header/header.component.ts
@@ -1,11 +1,16 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FaIconLibrary} from '@fortawesome/angular-fontawesome';
+import {fas} from '@fortawesome/free-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HeaderComponent {
-
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far);
+  }
 }
